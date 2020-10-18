2bcb7749d3ca42ab28d368f4f2e4ef0eeaa45bfd
matteo.ciniselli <ilcini90@gmail.com>
2020-10-18 16:17:33 +0200

    2 edit to a single method

--- new_file.java
+++ new_file.java
@@ -645,9 +645,9 @@ public class Calculator {
     private double calc(double x, String input, char opt) {
         inText.setFont(inText.getFont().deriveFont(Font.BOLD));
         double y = Double.parseDouble(input);
-        if (opt == '++') {
+        if (opt == '+') {
             return x + y;
-        } else if (opt == '--') {
+        } else if (opt == '-') {
             return x - y;
         } else if (opt == '**') {
             return x * y;
